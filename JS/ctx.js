452f47ca84754a487184ae050ed1a1d31ac1d981
Ingrid <mtra.ingrid.lopez@gmail.com>
2024-10-15 11:35:02 -0700

    Se creó el archivo de moment.js

--- JS/ctx.js
+++ JS/ctx.js
@@ -30,17 +30,3 @@ const data = {
     options: {}
   };
 
-
-
-new Chart(ctx, config);
-
-const now = moment();
-
-document.getElementById('formattedDate').innerHTML =
-'Fecha actual: ' + now;
-
-// Formatear la fecha en un formato más legible
-const formattedDate = now.format('MMMM Do YYYY, h:mm:ss a'); // Ejemplo: Octubre 14º 2024, 3:45:10 pm
-document.getElementById('formattedDate').innerHTML = 'Today is: ' + formattedDate;
-  
-
---
+++ JS/moment.js
@@ -0,0 +1,11 @@
+new Chart(ctx, config);
+
+const now = moment();
+
+document.getElementById('formattedDate').innerHTML =
+'Fecha actual: ' + now;
+
+// Formatear la fecha en un formato más legible
+const formattedDate = now.format('MMMM Do YYYY, h:mm:ss a'); // Ejemplo: Octubre 14º 2024, 3:45:10 pm
+document.getElementById('formattedDate').innerHTML = 'Today is: ' + formattedDate;
+  
